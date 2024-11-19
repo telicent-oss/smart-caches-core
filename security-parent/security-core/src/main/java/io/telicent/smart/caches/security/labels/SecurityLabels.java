80360c2cd4c70ee21807f53d3e2bd0cfd8eeb519
Rob Vesse <rob.vesse@telicent.io>
2024-11-19 13:27:50 +0000

    1st draft of Security Plugin API (CORE-207)
    Outlines the basic interfaces and model classes necessary for the
    Security Plugin proposal to help flesh out the design and identify areas
    where the paper sketch doesn't match implementable reality.
    
    Starts writing up documentation around it, from that work and additional
    tests written in support of documentation make some implementation
    changes to avoid generic signature issues when used by applications.

---
+++ security-parent/security-core/src/main/java/io/telicent/smart/caches/security/labels/SecurityLabels.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) Telicent Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.telicent.smart.caches.security.labels;
+
+import io.telicent.smart.caches.security.SecurityPrimitive;
+
+/**
+ * Interface for holding security labels
+ *
+ * @param <T> Decoded labels type
+ */
+public interface SecurityLabels<T> extends SecurityPrimitive {
+
+    /**
+     * The security label decoded into a suitable concrete representation for a security plugin to process
+     *
+     * @return Decoded label representation
+     */
+    T decodedLabels();
+
+    /**
+     * A human-readable format of the security labels intended for debugging purposes
+     * <p>
+     * This is intentionally a separate method instead of overriding {@link Object#toString()}, which an implementation
+     * may still choose to do so.  The idea here is that producing a human-readable debugging string may involve
+     * significantly more work than just producing a quick string representation, and we want to allow for both.  For
+     * example the {@link Object#toString()} override might just print the label schema and the size of the encoded
+     * label, whereas the implementation of this method should decode the label and render it in a way that better aids
+     * debugging.
+     * </p>
+     *
+     * @return Debugging representation of the label
+     */
+    String toDebugString();
+}
---
+++ security-parent/security-core/src/test/java/io/telicent/smart/caches/security/plugins/failsafe/TestFailSafePlugin.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) Telicent Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.telicent.smart.caches.security.plugins.failsafe;
+
+import io.telicent.smart.caches.security.entitlements.Entitlements;
+import io.telicent.smart.caches.security.entitlements.MalformedEntitlementsException;
+import io.telicent.smart.caches.security.plugins.AbstractSecurityPluginTests;
+import io.telicent.smart.caches.security.plugins.SecurityPlugin;
+import org.testng.annotations.DataProvider;
+
+public class TestFailSafePlugin extends AbstractSecurityPluginTests {
+    @Override
+    protected SecurityPlugin getPlugin() {
+        return FailSafePlugin.INSTANCE;
+    }
+
+    @DataProvider(name = "validLabels")
+    @Override
+    protected Object[][] validLabels() {
+        return new Object[0][];
+    }
+
+    @DataProvider(name = "invalidLabels")
+    @Override
+    protected Object[][] invalidLabels() {
+        return new Object[][] {
+                { new byte[0] },
+                { new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9} },
+                { new byte[1000]}
+        };
+    }
+
+    @DataProvider(name = "accessibleLabels")
+    @Override
+    protected Object[][] accessibleLabels() {
+        return new Object[0][];
+    }
+
+    @DataProvider(name = "forbiddenLabels")
+    @Override
+    protected Object[][] forbiddenLabels() {
+        return new Object[0][];
+    }
+
+    @Override
+    protected Entitlements<?> getTestEntitlements() throws MalformedEntitlementsException {
+        return new RawPrimitive(new byte[0]);
+    }
+}
