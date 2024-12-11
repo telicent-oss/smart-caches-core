2a16d915ff91e3206dd49f66a64095fd14b069ac
Rob Vesse <rob.vesse@telicent.io>
2024-12-11 14:19:27 +0000

    2nd Draft of Security Plugin API design (CORE-207)
    Co-authored-by: Paul Gallagher <132362215+TelicentPaul@users.noreply.github.com>
    Co-authored-by: Rob Walpole <rob.walpole@telicent.io>
    Co-authored-by: Rob Levi <rob.levi@telicent.io>
    
    - Add new RequestContext API
    - JAX-RS implementation of RequestContext API
    - Adds support for the new Security Plugin RequestContext into the JAX-RS
      Base Server module including some basic test coverage of its
      functionality
    - Retrieving Entitlements is now based on passing in a RequestContext so
      the plugin has more information to work with.
    - Authorizer now has separate canRead(), canWrite() and canUse() methods
      for making different kinds of access decisions.

---
+++ security-parent/security-core/src/main/java/io/telicent/smart/cache/security/plugins/failsafe/FailSafeAuthorizer.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) Telicent Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.telicent.smart.cache.security.plugins.failsafe;
+
+import io.telicent.smart.cache.security.plugins.SecurityPlugin;
+import io.telicent.smart.cache.security.Authorizer;
+import io.telicent.smart.cache.security.entitlements.Entitlements;
+import io.telicent.smart.cache.security.labels.SecurityLabels;
+import io.telicent.smart.cache.security.requests.RequestContext;
+
+/**
+ * An authorizer that denies all access
+ * <p>
+ * This is intended primarily for use by the {@link FailSafePlugin} but may also be used be other plugins if they are
+ * asked to prepare an {@link Authorizer} via
+ * {@link SecurityPlugin#prepareAuthorizer(Entitlements)} instance using an
+ * {@link Entitlements} instance they do not support.
+ * </p>
+ */
+public final class FailSafeAuthorizer implements Authorizer {
+
+    /**
+     * Public singleton instance of the authorizer
+     */
+    public static final Authorizer INSTANCE = new FailSafeAuthorizer();
+
+    /**
+     * Private constructor to prevent direct instantiation, use {@link #INSTANCE} to obtain the singleton instance
+     * instead
+     */
+    private FailSafeAuthorizer() {
+
+    }
+
+    @Override
+    public boolean canRead(SecurityLabels<?> labels) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(SecurityLabels<?> labels) {
+        return false;
+    }
+
+    @Override
+    public boolean canUse(SecurityLabels<?> labels, RequestContext context) {
+        return false;
+    }
+
+    @Override
+    public void close() throws Exception {
+        // No-op
+    }
+}
---
+++ security-parent/security-plugin-rdf-abac/src/main/java/io/telicent/smart/cache/security/plugins/rdf/abac/RdfAbacLabels.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) Telicent Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.telicent.smart.cache.security.plugins.rdf.abac;
+
+import io.telicent.jena.abac.attributes.AttributeExpr;
+import io.telicent.smart.cache.security.AbstractSecurityPrimitive;
+import io.telicent.smart.cache.security.labels.SecurityLabels;
+
+import java.util.List;
+import java.util.Objects;
+
+public class RdfAbacLabels extends AbstractSecurityPrimitive implements SecurityLabels<List<AttributeExpr>> {
+
+    private final List<AttributeExpr> expressions;
+
+    RdfAbacLabels(byte[] encoded, List<AttributeExpr> expressions) {
+        super(RdfAbacPlugin.SCHEMA, encoded);
+        this.expressions = Objects.requireNonNull(expressions);
+    }
+
+    @Override
+    public List<AttributeExpr> decodedLabels() {
+        return this.expressions;
+    }
+
+    @Override
+    public String toDebugString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < this.expressions.size(); i++) {
+            sb.append(this.expressions.get(i).toString());
+            if (i < this.expressions.size() - 1) {
+                sb.append(" && ");
+            }
+        }
+        return sb.toString();
+    }
+}
