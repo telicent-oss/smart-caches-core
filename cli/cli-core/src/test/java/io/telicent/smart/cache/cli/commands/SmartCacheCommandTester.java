15a5ce54de1366b48e46bef2001c6977b94d71ad
Rob Vesse <rob.vesse@telicent.io>
2025-02-24 11:38:57 +0000

    Support for testing external commands (CORE-705)
    Existing command testing focuses on running the command in the same JVM
    as the test.  This is useful in most cases but it doesn't catch
    dependency scoping errors that can creep in when something is placed
    into test scope unintentionally.
    
    This commit adds new support to SmartCacheCommandTester for running
    tests where the command is spawned as it would be in a real deployment.
    This allows for easily writing test cases that can verify the real
    application will run and have all the relevant dependencies available to
    actually do its job correctly in a real deployment.

--- cli/cli-core/src/test/java/io/telicent/smart/cache/cli/commands/SmartCacheCommandTester.java
+++ cli/cli-core/src/test/java/io/telicent/smart/cache/cli/commands/SmartCacheCommandTester.java
@@ -1,28 +1,29 @@
 /**
  * Copyright (C) Telicent Ltd
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package io.telicent.smart.cache.cli.commands;
 
 import com.github.rvesse.airline.parser.ParseResult;
+import io.telicent.smart.cache.projectors.utils.WriteOnceReference;
 import org.apache.commons.io.output.NullOutputStream;
 import org.apache.commons.io.output.TeeOutputStream;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Provides the ability to put {@link SmartCacheCommand} derived command classes into a test mode in order to allow
@@ -34,13 +35,16 @@ public class SmartCacheCommandTester {
     private static final ByteArrayOutputStream LAST_OUTPUT = new ByteArrayOutputStream(), LAST_ERROR =
             new ByteArrayOutputStream();
 
-    private static File LAST_OUTPUT_FILE = null, LAST_ERROR_FILE = null;
+    private static File LAST_OUTPUT_FILE = null, LAST_ERROR_FILE = null, EXTERNAL_OUTPUT_FILE = null,
+            EXTERNAL_ERROR_FILE = null;
 
     /**
      * When set to true the captured output and error from each test is also tee'd to the original standard output and
      * error, when set to false it is also captured to files for later review
      */
     public static boolean TEE_TO_ORIGINAL_STREAMS = false;
+
+    private static final WriteOnceReference<String> PROJECT_VERSION = new WriteOnceReference<>();
 
     private SmartCacheCommandTester() {
 
@@ -99,6 +103,8 @@ public class SmartCacheCommandTester {
         SmartCacheCommand.LAST_EXIT_STATUS = Integer.MIN_VALUE;
         LAST_OUTPUT.reset();
         LAST_ERROR.reset();
+        EXTERNAL_OUTPUT_FILE = null;
+        EXTERNAL_ERROR_FILE = null;
 
         if (!TEE_TO_ORIGINAL_STREAMS) {
             Path target = Path.of("target");
@@ -167,7 +173,16 @@ public class SmartCacheCommandTester {
      * @return Standard output
      */
     public static String getLastStdOut() {
-        return LAST_OUTPUT.toString(StandardCharsets.UTF_8);
+        if (EXTERNAL_OUTPUT_FILE != null) {
+            try {
+                return Files.readString(EXTERNAL_OUTPUT_FILE.toPath());
+            } catch (IOException e) {
+                throw new IllegalStateException(
+                        "Failed to read external command output file " + EXTERNAL_OUTPUT_FILE.getAbsolutePath(), e);
+            }
+        } else {
+            return LAST_OUTPUT.toString(StandardCharsets.UTF_8);
+        }
     }
 
     /**
@@ -176,6 +191,125 @@ public class SmartCacheCommandTester {
      * @return Standard error
      */
     public static String getLastStdErr() {
-        return LAST_ERROR.toString(StandardCharsets.UTF_8);
+        if (EXTERNAL_ERROR_FILE != null) {
+            try {
+                return Files.readString(EXTERNAL_ERROR_FILE.toPath());
+            } catch (IOException e) {
+                throw new IllegalStateException(
+                        "Failed to read external command output file " + EXTERNAL_ERROR_FILE.getAbsolutePath(), e);
+            }
+        } else {
+            return LAST_ERROR.toString(StandardCharsets.UTF_8);
+        }
+    }
+
+    /**
+     * Runs a command to test as an external standalone process, this is useful for writing integration tests that
+     * verify that the script entrypoint used for real deployments is functioning as intended and any necessary
+     * dependencies are appropriately provided.
+     * <p>
+     * This returns the actual process, depending on the command being tested you may then want to pass this into
+     * {@link #waitForExternalCommand(Process, long, TimeUnit)} to wait for it to complete, or to run tests against the
+     * running application before terminating it yourself.
+     * </p>
+     *
+     * @param program Program that should be invoked, typically a script file that is the entrypoint for the deployed
+     *                application
+     * @param envVars Any environment variables that need to be set for the program being tested to function correctly
+     * @param args    Any arguments to supply to the program
+     * @return The external command process
+     * @throws IOException Thrown if the external command process cannot be started e.g. bad program path
+     */
+    public static Process runAsExternalCommand(String program, Map<String, String> envVars, String[] args) throws
+            IOException {
+        // Prepare the process command
+        ProcessBuilder builder = new ProcessBuilder();
+        List<String> command = new ArrayList<>();
+        command.add(program);
+        command.addAll(Arrays.asList(args));
+        builder.command(command);
+
+        // Since we've already redirected stdout and stderr can just inherit IO from this process
+        EXTERNAL_OUTPUT_FILE = Files.createTempFile("external-stdout", ".txt").toFile();
+        EXTERNAL_ERROR_FILE = Files.createTempFile("external-stderr", ".txt").toFile();
+        builder.redirectOutput(EXTERNAL_OUTPUT_FILE);
+        builder.redirectError(EXTERNAL_ERROR_FILE);
+
+        // Customise environment
+        builder.environment().putAll(envVars);
+
+        printToOriginalStdOut(
+                "Starting external command " + program + " with arguments: " + StringUtils.join(args, "\n  "));
+        return builder.start();
+    }
+
+    /**
+     * Waits for the external command to complete, recording its exit status for later retrieval by
+     * {@link #getLastExitStatus()}.  The process is destroyed after it has completed, or if it does not complete within
+     * the given timeout duration.
+     *
+     * @param process Process
+     * @param timeout Timeout duration
+     * @param unit    Timeout time unit
+     */
+    public static void waitForExternalCommand(Process process, long timeout, TimeUnit unit) {
+        Objects.requireNonNull(process);
+        // Wait for completion
+        try {
+            printToOriginalStdOut("Waiting for external command to complete...");
+            process.waitFor(timeout, unit);
+            if (process.isAlive()) {
+                SmartCacheCommand.LAST_EXIT_STATUS = Integer.MAX_VALUE;
+                printToOriginalStdOut(
+                        "External command failed to finish within timeout (" + timeout + " " + unit.name() + ")");
+            } else {
+                SmartCacheCommand.LAST_EXIT_STATUS = process.exitValue();
+                printToOriginalStdOut(
+                        "External command completed with status " + SmartCacheCommand.LAST_EXIT_STATUS);
+            }
+        } catch (InterruptedException e) {
+            SmartCacheCommand.LAST_EXIT_STATUS = Integer.MAX_VALUE;
+        } finally {
+            process.destroy();
+        }
+    }
+
+    /**
+     * Detects the project version, this can be used to inject it into an external command as an environment variable
+     * (e.g. when using {@link #runAsExternalCommand(String, Map, String[])})
+     * <p>
+     * Detection looks for a {@code project.version} System property, following by reading the {@code pom.xml} file to
+     * find the first {@code <version>} element.
+     * </p>
+     * <p>
+     * The detected project version is cached for the lifetime of the JVM.
+     * </p>
+     *
+     * @return Detected project version, or {@code null} if unable to detect
+     */
+    public static String detectProjectVersion() {
+        return PROJECT_VERSION.computeIfAbsent(() -> {
+            // If injected via System Properties just return that
+            if (System.getProperties().contains("project.version")) {
+                return System.getProperties().getProperty("project.version");
+            }
+
+            // Otherwise read the first <version> element in the pom.xml
+            File pom = new File("pom.xml");
+            List<String> lines;
+            try {
+                lines = Files.readAllLines(pom.toPath());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to read project pom.xml file", e);
+            }
+            for (String line : lines) {
+                if (line.contains("<version>")) {
+                    line = StringUtils.substringAfter(line, "<version>");
+                    line = StringUtils.substringBefore(line, "</version>");
+                    return StringUtils.trim(line);
+                }
+            }
+            throw new IllegalStateException("Failed to detect project version");
+        });
     }
 }
