1a9f70d5a36467228c9908843d74afc1cd5bf0f0
Rob Vesse <rob.vesse@telicent.io>
2025-09-09 10:37:42 +0100

    Define annotations for Authorization (CORE-951)
    Adds several new annotations that can be used to annotate JAX-RS
    resource methods and classes to indicate the roles and permissions
    required to access the endpoints represented by those.  With helper code
    for locating the most specific annotation that applies to a given
    resource method.

---
+++ jwt-auth-common/src/main/java/io/telicent/smart/caches/configuration/auth/annotations/AnnotationLocator.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (C) Telicent Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.telicent.smart.caches.configuration.auth.annotations;
+
+import lombok.NoArgsConstructor;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+@NoArgsConstructor
+public class AnnotationLocator {
+
+    /**
+     * Finds the most specific annotation for the method, or it's containing class, or any parent class
+     * <p>
+     * Finds the annotation that occurs at the most specific level i.e. prefers method annotations over class
+     * annotations, and prefers class annotations lower down the inheritance tree.
+     * </p>
+     *
+     * @param method              Method
+     * @param eligibleAnnotations Eligible annotations to find in order of precedence, if multiple of these annotations
+     *                            are present then only the first found is returned so callers should take care to
+     *                            provide their desired precedence order
+     * @return Annotation, or {@code null} if no such annotations
+     */
+    private static Annotation findMostSpecific(Method method, Class<? extends Annotation>[] eligibleAnnotations) {
+        if (method == null) {
+            return null;
+        }
+
+        // Check whether the method is directly annotated with any of the given annotations
+        for (Class<? extends Annotation> eligible : eligibleAnnotations) {
+            if (method.isAnnotationPresent(eligible)) {
+                return method.getAnnotation(eligible);
+            }
+        }
+
+        // If not annotated on the method see if there are any annotations at the class level
+        return findMostSpecific(method.getDeclaringClass(), eligibleAnnotations);
+    }
+
+    /**
+     * Finds the most specific annotation for the class, or any of its parent classes
+     * <p>
+     * Finds the annotation that occurs at the most specific level i.e. prefers method annotations over class *
+     * annotations, and prefers class annotations lower down the inheritance tree.
+     * </p>
+     *
+     * @param clazz               Class
+     * @param eligibleAnnotations Eligible annotations to find in order of precedence, if multiple of these annotations
+     *                            are present then only the first found is returned so callers should take care to
+     *                            provide their desired precedence order
+     * @return Annotation, or {@code null} if no such annotations
+     */
+    private static Annotation findMostSpecific(Class<?> clazz, Class<? extends Annotation>[] eligibleAnnotations) {
+        if (clazz == null) {
+            return null;
+        }
+
+        // Check whether the class is annotated with any of the given annotations
+        for (Class<? extends Annotation> eligible : eligibleAnnotations) {
+            if (clazz.isAnnotationPresent(eligible)) {
+                return clazz.getAnnotation(eligible);
+            }
+        }
+
+        // Walk up the type hierarchy to see if there are any annotations on a parent class
+        if (clazz.getSuperclass() != null) {
+            return findMostSpecific(clazz.getSuperclass(), eligibleAnnotations);
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Finds the strictest and most specific roles annotation present on the given method, its containing class, or a
+     * parent class
+     *
+     * @param method Method
+     * @return Strictest and most specific role annotation i.e. one of {@link DenyAll}, {@link RolesAllowed} or
+     * {@link PermitAll}, or {@code null} if no such annotations
+     */
+    @SuppressWarnings("unchecked")
+    public static Annotation findRoleAnnotation(Method method) {
+        return findMostSpecific(method, new Class[] { DenyAll.class, RolesAllowed.class, PermitAll.class });
+    }
+
+    /**
+     * Finds the most specific permissions annotations present on the given method, its containing class, or a parent
+     * class
+     *
+     * @param method Method
+     * @return Most specific permissions annotation present i.e. {@link RequirePermissions}, or {@code null} if no such
+     * annotations
+     */
+    @SuppressWarnings("unchecked")
+    public static Annotation findPermissionsAnnotation(Method method) {
+        return findMostSpecific(method, new Class[] { RequirePermissions.class });
+    }
+}
---
+++ jwt-auth-common/src/test/java/io/telicent/smart/caches/configuration/auth/annotations/TestAnnotationLocator.java
@@ -0,0 +1,163 @@
+/**
+ * Copyright (C) Telicent Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.telicent.smart.caches.configuration.auth.annotations;
+
+import io.telicent.smart.caches.configuration.auth.annotations.examples.ExampleAdmin;
+import io.telicent.smart.caches.configuration.auth.annotations.examples.ExampleBase;
+import io.telicent.smart.caches.configuration.auth.annotations.examples.ExampleUser;
+import io.telicent.smart.caches.configuration.auth.annotations.examples.Nothing;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public class TestAnnotationLocator {
+
+    @Test
+    public void givenClassWithNoAnnotations_whenLocatingAnnotations_thenNull() throws NoSuchMethodException {
+        // Given
+        Nothing nothing = new Nothing();
+        Method method = nothing.getClass().getMethod("doNothing");
+
+        // When
+        Annotation roles = AnnotationLocator.findRoleAnnotation(method);
+
+        // Then
+        Assert.assertNull(roles);
+    }
+
+    private void verifyPermitAll(Annotation annotation) {
+        Assert.assertTrue(annotation instanceof PermitAll);
+    }
+
+    private void verifyDenyAll(Annotation annotation) {
+        Assert.assertTrue(annotation instanceof DenyAll);
+    }
+
+    private void verifyRoles(Annotation annotation, String... expected) {
+        Assert.assertTrue(annotation instanceof RolesAllowed);
+        RolesAllowed rolesAllowed = (RolesAllowed) annotation;
+        Assert.assertEquals(rolesAllowed.value(), expected);
+    }
+
+    private void verifyPermissions(Annotation annotation, String... expected) {
+        Assert.assertTrue(annotation instanceof RequirePermissions);
+        RequirePermissions requirePermissions = (RequirePermissions) annotation;
+        Assert.assertEquals(requirePermissions.value(), expected);
+    }
+
+    @Test
+    public void givenClassWithAnnotations_whenLocatingAnnotationsOnMethodWithoutAnnotation_thenClassAnnotationReturned() throws
+            NoSuchMethodException {
+        // Given
+        ExampleBase example = new ExampleBase();
+        Method method = example.getClass().getMethod("defaults");
+
+        // When
+        Annotation roles = AnnotationLocator.findRoleAnnotation(method);
+
+        // Then
+        verifyRoles(roles, "USER", "ADMIN");
+    }
+
+    @Test
+    public void givenClassWithAnnotations_whenLocatingAnnotationsOnMethodWithAnnotation_thenMethodAnnotationReturned() throws
+            NoSuchMethodException {
+        // Given
+        ExampleBase example = new ExampleBase();
+        Method method = example.getClass().getMethod("allowAll");
+
+        // When
+        Annotation roles = AnnotationLocator.findRoleAnnotation(method);
+
+        // Then
+        verifyPermitAll(roles);
+    }
+
+    @Test
+    public void givenClassWithAnnotations_whenLocatingAnnotationsOnMethodWithMultipleAnnotation_thenStrictestAnnotationReturned() throws
+            NoSuchMethodException {
+        // Given
+        ExampleBase example = new ExampleBase();
+        Method method = example.getClass().getMethod("denyAll");
+
+        // When
+        Annotation roles = AnnotationLocator.findRoleAnnotation(method);
+
+        // Then
+        verifyDenyAll(roles);
+    }
+
+    @Test
+    public void givenClassWithPermissionAnnotations_whenLocatingAnnotationsOnMethodWithoutAnnotations_thenClassAnnotationReturned() throws
+            NoSuchMethodException {
+        // Given
+        ExampleAdmin example = new ExampleAdmin();
+        Method method = example.getClass().getMethod("doAdmin");
+
+        // When
+        Annotation roles = AnnotationLocator.findRoleAnnotation(method);
+        Annotation perms = AnnotationLocator.findPermissionsAnnotation(method);
+
+        // Then
+        verifyRoles(roles, "ADMIN");
+        verifyPermissions(perms, "admin:write");
+    }
+
+    @Test
+    public void givenClassWithPermissionAnnotations_whenLocatingAnnotationsOnMethodWithAnnotations_thenMethodAnnotationReturned() throws
+            NoSuchMethodException {
+        // Given
+        ExampleAdmin example = new ExampleAdmin();
+        Method method = example.getClass().getMethod("reset");
+
+        // When
+        Annotation roles = AnnotationLocator.findRoleAnnotation(method);
+        Annotation perms = AnnotationLocator.findPermissionsAnnotation(method);
+
+        // Then
+        verifyRoles(roles, "SYS-ADMIN");
+        verifyPermissions(perms, "admin:write", "admin:wipe");
+    }
+
+    @DataProvider(name = "userPermissions")
+    private Object[][] permissions() {
+        return new Object[][] {
+                { "whoami", new String[] { "user:read" } },
+                { "updateDetails", new String[] { "user:write" } },
+                { "active", new String[] { "user:read", "user:write" } },
+                { "random", new String[0] }
+        };
+    }
+
+    @Test(dataProvider = "userPermissions")
+    public void givenClassWithPermissionAnnotations_whenLocatingAnnotationsOnMethod_thenExpectedPermissionsFound(String methodName, String[] expected) throws
+            NoSuchMethodException {
+        // Given
+        ExampleUser example = new ExampleUser();
+        Method method = example.getClass().getMethod(methodName);
+
+        // When
+        Annotation roles = AnnotationLocator.findRoleAnnotation(method);
+        Annotation perms = AnnotationLocator.findPermissionsAnnotation(method);
+
+        // Then
+        verifyRoles(roles, "USER", "ADMIN");
+        verifyPermissions(perms, expected);
+    }
+}
