fd0e8b51e47bd0ca325e449c5b3587bc7c907e6d
Rob Vesse <rob.vesse@telicent.io>
2026-05-06 13:44:55 +0100

    Add missing Javadoc (CORE-1275)

--- event-sources/event-sources-core/src/main/java/io/telicent/smart/cache/payloads/Either.java
+++ event-sources/event-sources-core/src/main/java/io/telicent/smart/cache/payloads/Either.java
@@ -17,22 +17,48 @@ package io.telicent.smart.cache.payloads;
 
 import lombok.Getter;
 
+/**
+ * A helper container class that allows a payload value to be represented as one of two possible types
+ *
+ * @param <A> Possible type A
+ * @param <B> Possible type B
+ */
 @Getter
 public final class Either<A, B> {
 
     private final A a;
     private final B b;
 
+    /**
+     * Creates a new container
+     * <p>
+     * Only one of the value parameters is permitted to be non-null, if both are null an error is thrown.
+     * </p>
+     *
+     * @param a Value
+     * @param b Value
+     * @throws IllegalArgumentException Thrown if both values are set
+     */
     public Either(A a, B b) {
         if (a != null && b != null) throw new IllegalArgumentException("Can't set both value types");
         this.a = a;
         this.b = b;
     }
 
+    /**
+     * Gets whether the container has a value of the first possible type
+     *
+     * @return True if the first possible type, false otherwise
+     */
     public boolean isA() {
         return this.a != null;
     }
 
+    /**
+     * Gets whether the container has a value of the second possible type
+     *
+     * @return True if the second possible type, false otherwise
+     */
     public boolean isB() {
         return this.b != null;
     }
