a320844e671c4284eef64748d78a76c2febe7cc5
Rob Vesse <rob.vesse@telicent.io>
2026-05-06 19:37:37 +0100

    Add state machine enums (CORE-1275,CORE-1282)
    Adds the basic state machine enum types and legal transition tests to
    the distribution-lifecycle module

---
+++ distribution-lifecycle/src/main/java/io/telicent/smart/cache/distribution/lifecycle/DistributionLifecycleState.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (C) Telicent Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.telicent.smart.cache.distribution.lifecycle;
+
+/**
+ * Possible Distribution Lifecycle states
+ */
+public enum DistributionLifecycleState {
+
+    /**
+     * Distribution is not yet known to the platform, its data <strong>MUST NOT</strong> be ingested.
+     */
+    Unregistered,
+    /**
+     * Distribution is registered with the platform, its data <strong>MUST</strong> be accepted for ingest but
+     * <strong>MUST NOT</strong> be visible to users.
+     */
+    Registered,
+    /**
+     * Distribution is active, its data <strong>MUST</strong> be accepted for ingest and <strong>MUST</strong> be
+     * visible to suitably authorised users.
+     */
+    Active,
+    /**
+     * Distribution is withdrawn, its data <strong>MUST</strong> continue to be accepted for ingest but
+     * <strong>MUST NOT</strong> be visible to end users.
+     */
+    Withdrawn,
+    /**
+     * Distribution is deleted, its data <strong>MUST NOT</strong> be ingested and <strong>MUST NOT</strong> be visible
+     * to users.  Also, a lifecycle aware service <strong>MUST</strong> actively start removing data belonging to that
+     * distribution from its persistent storage.
+     */
+    Deleted;
+
+    /**
+     * Checks whether a transition from the current state to another is permitted
+     *
+     * @param target  Target state
+     * @return True if legal transition, false otherwise
+     */
+    public boolean canTransition(DistributionLifecycleState target) {
+        return canTransition(this, target);
+    }
+
+    /**
+     * Checks whether a transition from one state to another is permitted
+     *
+     * @param current Current state
+     * @param target  Target state
+     * @return True if legal transition, false otherwise
+     */
+    public static boolean canTransition(DistributionLifecycleState current, DistributionLifecycleState target) {
+        return switch (current) {
+            // An Unregistered distribution can only transition to Registered
+            case Unregistered -> target == Registered;
+            // A Registered/Withdrawn distribution can either transition to Active/Deleted
+            case Registered, Withdrawn -> target == Active || target == Deleted;
+            // An Active distribution can transition to Withdrawn/Deleted
+            case Active -> target == Withdrawn || target == Deleted;
+            // A Deleted distribution is permanently deleted and cannot transition to another state
+            case Deleted -> false;
+        };
+    }
+}
---
+++ distribution-lifecycle/src/test/java/io/telicent/smart/cache/distribution/lifecycle/TestApplicationState.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) Telicent Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.telicent.smart.cache.distribution.lifecycle;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestApplicationState {
+
+    @DataProvider
+    public Object[][] validTransitions() {
+        // The manually curated list of legal transitions, if you change ApplicationState#canTransition() in
+        // any way then this MUST be appropriately updated
+        return new Object[][] {
+                { ApplicationState.Requested, ApplicationState.InProgress },
+                { ApplicationState.InProgress, ApplicationState.Completed },
+                { ApplicationState.InProgress, ApplicationState.Failed },
+                { ApplicationState.Failed, ApplicationState.InProgress}
+        };
+    }
+
+    @DataProvider
+    public Object[][] invalidTransitions() {
+        // Calculate this as the negation of the validTransitions()
+        List<Object[]> legalTransitions = Arrays.stream(validTransitions()).toList();
+        List<Object[]> illegalTransitions = new ArrayList<>();
+        for (ApplicationState source : ApplicationState.values()) {
+            for (ApplicationState target : ApplicationState.values()) {
+                if (legalTransitions.stream().anyMatch(t -> t[0] == source && t[1] == target)) {
+                    continue;
+                }
+                illegalTransitions.add(new Object[] { source, target });
+            }
+        }
+        return illegalTransitions.toArray(new Object[0][]);
+    }
+
+    @Test(dataProvider = "validTransitions")
+    public void givenValidTransition_whenCheckingLegality_thenOk(ApplicationState source,
+                                                                 ApplicationState target) {
+        // Given and When
+        boolean legal = ApplicationState.canTransition(source, target);
+
+        // Then
+        Assert.assertTrue(legal);
+    }
+
+    @Test(dataProvider = "validTransitions")
+    public void givenValidTransition_whenCheckingLegalityOnSourceState_thenOk(ApplicationState source,
+                                                                 ApplicationState target) {
+        // Given and When
+        boolean legal = source.canTransition(target);
+
+        // Then
+        Assert.assertTrue(legal);
+    }
+
+    @Test(dataProvider = "invalidTransitions")
+    public void givenInvalidTransition_whenCheckingLegality_thenFails(ApplicationState source,
+                                                                      ApplicationState target) {
+        // Given and When
+        boolean legal = ApplicationState.canTransition(source, target);
+
+        // Then
+        Assert.assertFalse(legal);
+    }
+
+    @Test(dataProvider = "invalidTransitions")
+    public void givenInvalidTransition_whenCheckingLegalityOnSourceState_thenFails(ApplicationState source,
+                                                                      ApplicationState target) {
+        // Given and When
+        boolean legal = source.canTransition(target);
+
+        // Then
+        Assert.assertFalse(legal);
+    }
+}
